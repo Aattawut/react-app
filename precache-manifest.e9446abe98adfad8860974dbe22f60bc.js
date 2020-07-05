8e585cdbfdb28f9f9eab2fc3c648707ef193c526
Aattawut Nutlamyong <Aattawut.field@gmail.com>
2020-07-05 23:28:54 +0700

    Updates

---
+++ precache-manifest.e9446abe98adfad8860974dbe22f60bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d44ce72a23da721685132af012c690b",
+    "url": "/front-end/index.html"
+  },
+  {
+    "revision": "60d64206d46536863a26",
+    "url": "/front-end/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6316f59356380cfb1f10",
+    "url": "/front-end/static/js/2.401179c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/front-end/static/js/2.401179c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d64206d46536863a26",
+    "url": "/front-end/static/js/main.50c44ff2.chunk.js"
+  },
+  {
+    "revision": "c471e1b88722936290b8",
+    "url": "/front-end/static/js/runtime-main.0e928077.js"
+  }
+]);
